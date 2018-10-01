cdf7445600726920c9bfd56064307fb7bda376eb
JonathanMH0716 <JonathanMH0716@10-LTOP-WS-05A>
2018-10-01 08:37:19 -0500

    Music Store Home Store Controllers

---
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: /Store/
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        // Get: /Store/Browse/
+        public string Browse()
+        {
+            return "Hello from Store.Browse()";
+        }
+
+        // Get" /Store/Details/
+        public string Details()
+        {
+            return "Hello from Store.Details()";
+        }
+    }
+}
