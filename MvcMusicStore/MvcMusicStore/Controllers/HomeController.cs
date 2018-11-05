23423c9f774c1daeaa38fa1c9282a212fac2fff8
JonathanMH0716 <JonathanMH0716@10-LTOP-WS-05A>
2018-11-04 19:25:41 -0600

    Getting ready for review

--- MvcMusicStore/MvcMusicStore/Controllers/HomeController.cs
+++ MvcMusicStore/MvcMusicStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcMusicStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,26 @@ namespace MvcMusicStore.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        MusicStoreEntities storeDB = new MusicStoreEntities();
         public ActionResult Index()
         {
-            return View();
+
+            var albums = GetTopSellingAlbums(5);
+
+            return View(albums);
         }
+
+        private List<Album> GetTopSellingAlbums(int count)
+        {
+            // Group the order details by album and return
+            // the albums with the highest count
+            return storeDB.Albums
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToList();
+        }
+
     }
+
+
 }
